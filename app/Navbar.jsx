47e035e1966613be40073e4452e678222054a538
shadow6444 <shadowgarden644@gmail.com>
2024-10-18 14:25:33 +0500

    Build the Navbar

---
+++ app/Navbar.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+const Navbar = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b px-5 mb-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className="text-zinc-500 hover:text-zinc-800"
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;

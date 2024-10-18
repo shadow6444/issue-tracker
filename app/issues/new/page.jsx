b510bccbd78adbb3c7059e6196bea267f1beb289
shadow6444 <shadowgarden644@gmail.com>
2024-10-18 19:26:40 +0500

    Customize Radix UI theme

--- app/issues/new/page.jsx
+++ app/issues/new/page.jsx
@@ -1,12 +1,18 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextArea } from "@radix-ui/themes";
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title">
-        <TextField.Slot />
-      </TextField.Root>
-      <TextArea placeholder="Description" />
+      <div>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Title"
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        />
+      </div>
+      <TextArea placeholder="Description" className="font-inter"/>
       <Button>Submit New Issue</Button>
     </div>
   );

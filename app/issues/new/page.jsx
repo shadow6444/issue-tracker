ed0e1420d57054958652cefcd061652d17da20f5
shadow6444 <shadowgarden644@gmail.com>
2024-10-18 19:33:54 +0500

    Add a Markdown Editor

--- app/issues/new/page.jsx
+++ app/issues/new/page.jsx
@@ -1,8 +1,11 @@
-import { Button, TextArea } from "@radix-ui/themes";
+'use client'
 
+import { Button } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 const NewIssuePage = () => {
   return (
-    <div className="max-w-xl space-y-3">
+    <div className="max-w-xl space-y-3">  
       <div>
         <input
           type="text"
@@ -12,7 +15,7 @@ const NewIssuePage = () => {
           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
         />
       </div>
-      <TextArea placeholder="Description" className="font-inter"/>
+      <SimpleMDE placeholder="Description"/>
       <Button>Submit New Issue</Button>
     </div>
   );

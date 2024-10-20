ce8404357fc5dcae7f193cd11087e40d3d624229
shadow6444 <shadowgarden644@gmail.com>
2024-10-20 21:11:28 +0500

    Show a confirmation dialog box

---
+++ app/issues/[id]/DeleteIssueButton.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+
+const DeleteIssueButton = ({ issueId }) => {
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be
+          undone.
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
+
+export default DeleteIssueButton;
--- app/issues/[id]/page.jsx
+++ app/issues/[id]/page.jsx
@@ -1,8 +1,9 @@
 import prisma from "@/prisma/client";
-import { Box, Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
 
 const IssueDetailsPage = async ({ params }) => {
   const issue = await prisma.issue.findUnique({
@@ -11,12 +12,15 @@ const IssueDetailsPage = async ({ params }) => {
 
   if (!issue) notFound();
   return (
-    <Grid columns={{ initial: "1", md: "2" }} gap="5">
-      <Box>
+    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+      <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <EditIssueButton issueId={params.id} />
+        <Flex direction='column' gap='4'>
+          <EditIssueButton issueId={params.id} />
+          <DeleteIssueButton issueId={params.id} />
+        </Flex>
       </Box>
     </Grid>
   );

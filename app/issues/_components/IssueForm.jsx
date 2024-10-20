f6f360682e7670dec34848f5059e481ca6c88436
shadow6444 <shadowgarden644@gmail.com>
2024-10-20 20:48:31 +0500

    Improve the loading experience

--- app/issues/_components/IssueForm.jsx
+++ app/issues/_components/IssueForm.jsx
@@ -6,15 +6,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
+import SimpleMDE from "react-simplemde-editor";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { RiInformation2Line } from "react-icons/ri";
 
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
 
 const IssueForm = ({ issue }) => {
   const {

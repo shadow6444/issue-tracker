4dd823f6ca5463f1931a8eef41409bc257af7175
shadow6444 <shadowgarden644@gmail.com>
2024-10-20 20:48:31 +0500

    Improve the loading experience

--- app/issues/[id]/edit/page.jsx
+++ app/issues/[id]/edit/page.jsx
@@ -1,6 +1,12 @@
-import React from "react";
-import IssueForm from "../../_components/IssueForm";
 import prisma from "@/prisma/client";
+import IssueFormSkeleton from "./loading";
+import dynamic from "next/dynamic";
+import { notFound } from "next/navigation";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 const EditIssuePage = async ({ params }) => {
   const issue = await prisma.issue.findUnique({
--- app/issues/new/loading.jsx
+++ app/issues/new/loading.jsx
@@ -1,13 +1,3 @@
-import { Skeleton } from "@/app/components";
-import { Box } from "@radix-ui/themes";
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
 
-const LoadingNewIssue = () => {
-  return (
-    <Box className="max-w-xl">
-      <Skeleton />
-      <Skeleton height="20rem" />
-    </Box>
-  );
-};
-
-export default LoadingNewIssue;
+export default IssueFormSkeleton;

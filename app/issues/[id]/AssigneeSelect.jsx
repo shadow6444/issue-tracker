5154d1ee9377b7710b217acea0b795df67432a35
shadow6444 <shadowgarden644@gmail.com>
2024-10-21 19:11:39 +0500

    Populate the assignee select component

--- app/issues/[id]/AssigneeSelect.jsx
+++ app/issues/[id]/AssigneeSelect.jsx
@@ -1,15 +1,31 @@
-'use client'
+"use client";
 
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const {data} = await axios.get("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestion</Select.Label>
-          <Select.Item value="1">Shadow</Select.Item>
+          {users &&
+            users.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>

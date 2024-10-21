854c299df902a64072dff00b763b06ed89503884
shadow6444 <shadowgarden644@gmail.com>
2024-10-21 23:50:24 +0500

    Build the issue summary component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
-const Home = () => {
-  return <LatestIssues />;
+const Home = async () => {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 };
 
 export default Home;

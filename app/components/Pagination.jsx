649a319ecee9c5fc01c272b6e069d9ec54148dcb
shadow6444 <shadowgarden644@gmail.com>
2024-10-21 22:41:01 +0500

    Implement Pagination

--- app/components/Pagination.jsx
+++ app/components/Pagination.jsx
@@ -1,27 +1,61 @@
+"use client";
+
 import { Button, Flex, Text } from "@radix-ui/themes";
-import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
-import { MdKeyboardArrowLeft } from "react-icons/md";
-import { MdKeyboardDoubleArrowRight } from "react-icons/md";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import { useRouter, useSearchParams } from "next/navigation";
+import {
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight,
+} from "react-icons/md";
 
 const Pagination = ({ itemCount, pageSize, currentPage }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const pageCount = Math.ceil(itemCount / pageSize);
+
+  const changePage = (page) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
 
   return (
     <Flex align="center" gap="3">
-      <Text size='3'>
+      <Text size="3">
         Page {currentPage} of {pageCount}
       </Text>
-      <Button variant="soft" color="gray" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <MdKeyboardDoubleArrowLeft />
       </Button>
-      <Button variant="soft" color="gray">
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <MdKeyboardArrowLeft />
       </Button>
-      <Button variant="soft" color="gray">
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <MdKeyboardArrowRight />
       </Button>
-      <Button variant="soft" color="gray" disabled={currentPage === pageCount}>
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <MdKeyboardDoubleArrowRight />
       </Button>
     </Flex>
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
 import Pagination from "./components/Pagination";
 
-const Home = () => {
-  return <div><Pagination itemCount={100} pageSize={10} currentPage={10}/></div>;
+const Home = ({ searchParams }) => {
+  return (
+    <div>
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
+    </div>
+  );
 };
 
 export default Home;
